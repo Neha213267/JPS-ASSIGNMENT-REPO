dc2f623477b90f192c3461ebd885b5d2bb3b1edc
Neha213267 <80880578+Neha213267@users.noreply.github.com>
2022-02-20 20:17:08 +0530

    Add files via upload

---
+++ Core Java 11/Strings/Assignment3Q2.java
@@ -0,0 +1,10 @@
+
+public class Assignment3Q2 {
+    public static void main(String args[]){
+        
+        String str="Hello";
+        str=str.concat("How are You?");
+        System.out.println(str);
+    }
+    
+}
