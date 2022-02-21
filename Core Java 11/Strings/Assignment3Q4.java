574f01ab0fd3fff8b20ce9e5818044bd9b4475c1
Neha213267 <80880578+Neha213267@users.noreply.github.com>
2022-02-21 18:21:42 +0530

    Add files via upload

---
+++ Core Java 11/Strings/Assignment3Q4.java
@@ -0,0 +1,14 @@
+
+public class Assignment3Q4 
+{
+	 public static void main(String[] args)
+	 {
+	 StringBuffer s = new StringBuffer("");
+	 System.out.println(s);
+	 s.append("StringBuffer");
+	 s.append(" Is a peer class of String");
+	 s.append(" That provides much of");
+	 s.append(" The functionality of strings");
+	 System.out.println(s);
+     }
+    }
