832e1b4b4b6ed8be753088516e4bd4fa56ca46ab
Neha213267 <80880578+Neha213267@users.noreply.github.com>
2022-03-13 21:06:24 +0530

    Add files via upload

---
+++ Core Java 11/Strings/Assignment3Q1.java
@@ -0,0 +1,9 @@
+
+public class Assignment3Q1{
+    public static void main(String args[]){
+
+        String str1="Hello world";
+        
+        System.out.println("String length is " + str1.length());
+    }
+}
