a01f2f322ca7a2ef6ce19dce74ab4813eff2e080
Neha213267 <80880578+Neha213267@users.noreply.github.com>
2022-03-13 21:04:44 +0530

    Delete Core Java/Strings directory

--- Core Java/Strings/Assignment3Q1.java
+++
@@ -1,9 +0,0 @@
-
-public class Assignment3Q1{
-    public static void main(String args[]){
-
-        String str1="Hello world";
-        
-        System.out.println("String length is " + str1.length());
-    }
-}
