7c8c5a425ab12e5132d37510f9b390df957567da
Neha213267 <80880578+Neha213267@users.noreply.github.com>
2022-03-27 16:35:48 +0530

    Add files via upload

---
+++ Spring REST/SpringRestQ8/src/main/java/SpringRestq8Application.java
@@ -0,0 +1,13 @@
+package com.springRESTQ8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestq8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestq8Application.class, args);
+	}
+
+}
