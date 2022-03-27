1c48e4b404fc51a72f0d45dae6957ba0bca345d7
Neha213267 <80880578+Neha213267@users.noreply.github.com>
2022-03-27 14:35:24 +0530

    Add files via upload

---
+++ Spring REST/SpringRestQ7/src/main/java/SpringRestq7Application.java
@@ -0,0 +1,13 @@
+package com.springRESTQ7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestq7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestq7Application.class, args);
+	}
+
+}
