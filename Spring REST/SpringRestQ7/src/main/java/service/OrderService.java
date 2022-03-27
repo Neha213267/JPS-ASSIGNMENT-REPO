e41385197e640b5b648ab605aacb03571f885862
Neha213267 <80880578+Neha213267@users.noreply.github.com>
2022-03-27 14:34:46 +0530

    Add files via upload

---
+++ Spring REST/SpringRestQ7/src/main/java/service/OrderService.java
@@ -0,0 +1,37 @@
+package com.springRESTQ7.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springRESTQ7.model.Order;
+import com.springRESTQ7.repository.OrderRepo;
+
+
+@Service
+public class OrderService {
+	@Autowired
+    OrderRepo orderRepo;
+
+    public List<Order> getAllOrders(){
+        //System.out.println(orderRepo.findAll());
+        return  orderRepo.findAll();
+    }
+
+    public Order AddOrder(Order order){
+
+
+        return orderRepo.insert(order);
+    }
+    public  Order getOrderById(String id){
+        return  orderRepo.findById(id).get();
+    }
+    public  boolean deteleOrder(String id){
+        orderRepo.deleteById(id);
+        return true;
+    }
+    public  Order updateOrder(Order order){
+        return  orderRepo.save(order);
+    }
+}
