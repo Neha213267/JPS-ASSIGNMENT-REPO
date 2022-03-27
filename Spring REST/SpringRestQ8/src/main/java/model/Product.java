ac46bc9b32b4e69a5ae665319e2caae3f0088bd6
Neha213267 <80880578+Neha213267@users.noreply.github.com>
2022-03-27 16:34:31 +0530

    Add files via upload

---
+++ Spring REST/SpringRestQ8/src/main/java/model/Product.java
@@ -0,0 +1,33 @@
+package com.springRESTQ8.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "products")
+public class Product {
+	
+	@Id
+	private String id;
+    private String productName;
+    private Double productCost;
+    
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public Double getProductCost() {
+		return productCost;
+	}
+	public void setProductCost(Double productCost) {
+		this.productCost = productCost;
+	}
+    
+}
