5133ce69a7dff033563650b5742a74bc6926dfa4
Neha213267 <80880578+Neha213267@users.noreply.github.com>
2022-04-04 07:48:34 +0530

    Add files via upload

---
+++ Javascript/Basic/BasicJS.js
@@ -0,0 +1,16 @@
+//Q1
+var name = console.log("Hello World!"); 
+
+//Q2
+
+var name=window.prompt("Please Enter your name:")
+
+alert("Welcome "+ name);
+
+//Q3
+var name=window.prompt("Please Enter your name:")
+if(name=="alice" || name=="bob"){
+    alert("Welcome "+ name);
+}else{
+    alert("You are not Alice or Bob "+name +" You are not Welcome!");
+}
